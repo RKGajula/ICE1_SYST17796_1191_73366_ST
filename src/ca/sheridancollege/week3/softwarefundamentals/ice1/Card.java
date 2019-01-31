1a37b3236c2445c38be835dbf81ec2c0871e4821
Lorddddd <47199753+Lorddddd@users.noreply.github.com>
2019-01-31 15:34:17 -0500

    copy right date added

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -7,6 +7,7 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
 /**
  * A class that models playing card Objects. Cards have 
+ * jan 31
  * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
  * A suit (clubs, hearts, spades, diamonds).
  * There are 52 cards in a deck, no jokers.
